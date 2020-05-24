8e5eafa018308b64d2effb1ad72a6eaa76886a0e
Nahid Hasan Nirob <46191023+Nirob3x@users.noreply.github.com>
2020-05-25 04:49:48 +0800

    Create setupocservAuto.sh

---
+++ setupocservAuto.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+clear
+echo "   ___         _    ___  ___ ____   "
+echo "  / __|___ _ _| |_ / _ \/ __|__  |  "
+echo " | (__/ -_) ' \  _| (_) \__ \ / /   "
+echo "  \___\___|_||_\__|\___/|___//_/    "
+echo " OpenConnect VPN Server OneClick Install BY Nirob3x"
+
+echo "Point a Domain or Sub-Domain With Your VPS IP"
+ echo "Make Sure Your VPS is in Centos7 64x "
+echo ""
+echo "Inter Your Domain / SubDomain that pointed to VPS IP"
+read -p "Domain/Subdomain: " DomainTool
+echo ""
+echo "Finally, Input Your Email For Free SSL "
+echo "This Free SSL is powered by letsencrypt"
+read -p " Email: " EmailTool
+echo ""
+echo "Okay, that's all.Ready to setup your OpenConnect VPN Server Now?"
+read -n1 -r -p "Press any key to continue..."
+
+
+yum update -y
+yum install wget -y
+yum install dnf -y
+sudo dnf install epel-release -y
+sudo dnf install ocserv -y
+sudo yum install firewalld -y
+sudo systemctl enable firewalld
+sudo firewall-cmd --zone=public --permanent --add-port=443/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=443/udp
+sudo firewall-cmd --zone=public --permanent --add-port=10000/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=10000/udp
+sudo systemctl reload firewalld
+sudo dnf install certbot -y
+sudo certbot certonly --standalone --preferred-challenges http --agree-tos --email $EmailTool -d DomainTool
+
+
